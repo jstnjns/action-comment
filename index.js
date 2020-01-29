03c7e657994aa80ffc549cc46b4172b7a607f4aa
justinjones53@gmail.com <justinjones53@gmail.com>
2020-01-29 10:22:12 -0800

    getting hash in a pretty hacky way

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const github = require('@actions/github')
 const { map } = require('lodash')
 
 
+const parseHash = (str) => {
+  const expression = /\b[0-9a-f]{5,40}\b/
+
+  return str.match(expression)
+}
+
+
 async function run() {
   if (!github.context.payload.pull_request) {
     core.error('This action is only valid on Pull Requests')
@@ -16,13 +23,17 @@ async function run() {
   console.log('files', files)
 
   const comments = map((files || []), async (file) => {
+    const commit = parseHash(file.blob_url)
+
+    if (!commit) return
+
     const comment = {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      issue_number: github.context.payload.pull_request.number,
+      pull_number: github.context.payload.pull_request.number,
       body: core.getInput('comment'),
-      // commit_id: github.context.payload.after,
-      // path: file.filename,
+      commit_id: commit,
+      path: file.filename,
       // line: 0,
       // position: 0,
       // side: 'RIGHT',
