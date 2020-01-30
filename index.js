9b7769e1d4b394b409732fb518359d613a6d70f5
justinjones53@gmail.com <justinjones53@gmail.com>
2020-01-30 10:25:27 -0800

    issues?

--- index.js
+++ index.js
@@ -33,14 +33,14 @@ async function run() {
     const comment = {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      pull_number: github.context.payload.pull_request.number,
+      issue_number: github.context.payload.pull_request.number,
       body: core.getInput('comment'),
       commit_id: commit,
     }
 
     console.log('comment', comment)
     try {
-      await octokit.pulls.createComment(comment)
+      await octokit.issues.createComment(comment)
     } catch(err) {
       core.setFailed(err)
     }
