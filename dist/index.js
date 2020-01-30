7b7743d0263054c8699258ec187bbb955df5058a
justinjones53@gmail.com <justinjones53@gmail.com>
2020-01-30 10:54:48 -0800

    maybe?

--- dist/index.js
+++ dist/index.js
@@ -488,9 +488,8 @@ async function run() {
       body: core.getInput('comment'),
       commit_id: commit,
       path: file.filename,
-      position: 1,
       side: 'LEFT',
-      // line: 0,
+      line: 0,
     }
 
     console.log('comment', comment)
